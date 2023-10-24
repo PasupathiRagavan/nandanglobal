e0f078aae7f670a571d620625ea513a0978ce8ca
ravi ew <ravi.raya@gmail.com>
2023-10-24 16:43:33 +0530

    all task are completed

--- wp-content/themes/nandan-global/template-parts/content-page.php
+++ wp-content/themes/nandan-global/template-parts/content-page.php
@@ -8,7 +8,25 @@
  */
 
 ?>
+<?php if(is_front_page()): ?>
+	<?php the_content(); ?>
+<?php else : ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<section class="py-16">
+			<div class="container mx-auto">
+				<?php the_title( '<h1 class="entry-title text-3xl text-gray-900 font-bold entry-title mb-4">', '</h1>' ); ?>
+		
+					<?php nandan_global_post_thumbnail(); ?>
 
-<?php 
-	the_content();
-?>
+					<div class="entry-content description">
+						<?php
+							the_content();
+						?>
+					</div><!-- .entry-content -->
+
+			</div>
+		</section>
+	
+	</article><!-- #post-<?php the_ID(); ?> -->
+
+<?php endif; ?>
